a86e465db9cc23f85e37f674f4013939d231765f
AnujDev1000 <aj.jadhav2001@gmail.com>
2023-07-27 12:59:16 +0000

    Setup Commit (Anuj Jadhav)

---
+++ testngapp/src/test/java/ObjectClasses/SetupDriver.java
@@ -0,0 +1,33 @@
+package ObjectClasses;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+
+public class SetupDriver {
+    public static WebDriver driver=null;
+
+    @BeforeSuite(alwaysRun = true)
+    public static void setup() throws MalformedURLException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+        driver.get("https://artgallery.neohire.io/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // driver.get("https://artgallery.neohire.io/admin/login.php");
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+}
